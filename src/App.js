2c05056fac0a21597603b7af1f5884b1e0ca07db
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-12 16:50:36 -0300

    z

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./jplAvatar.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Proximamente sitio web de JPL</p>
+        <a
+          className="App-link"
+          href="https://github.com/JPLACLAU"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          JPL's Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
